fa2cbead2cc5fde4f23112d94c3707fc11b894bb
Lei Wang <tendant@gmail.com>
2020-05-04 21:00:03 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+
+set -ex
+
 git co --orphan gh-pages
+git reset
 helm package .
 helm repo index . --url https://tendant.github.io/simple-app
+git add index.yaml simple-app-0.1.0.tgz
+git commit -am "Update release"
+git clean -df
+# git push
