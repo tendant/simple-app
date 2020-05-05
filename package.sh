6ed03e5110783941cc8b3ab024b0b9d3e9535ac3
Lei Wang <tendant@gmail.com>
2020-05-05 13:51:38 -0700

    update package version

--- package.sh
+++ package.sh
@@ -2,13 +2,15 @@
 
 set -ex
 
+VERSION=$(grep -e ^version Chart.yaml | cut -d' ' -f 2)
+
 if [ -z "$(git status --porcelain)" ]; then
     git branch -D gh-pages
     git checkout --orphan gh-pages
     git reset
     helm package .
     helm repo index . --url https://tendant.github.io/simple-app
-    git add index.yaml simple-app-0.1.0.tgz
+    git add index.yaml simple-app-$VERSION.tgz
     git commit -am "Update release"
     git clean -df
     git push -f origin gh-pages
