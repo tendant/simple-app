721cc1a55aebe858349cd64be6bdbcc42a473a2e
Lei Wang <tendant@gmail.com>
2021-06-08 16:54:03 -0700

    add default latest version

--- package.sh
+++ package.sh
@@ -5,6 +5,7 @@ set -ex
 VERSION=$(grep -e ^version Chart.yaml | cut -d' ' -f 2)
 
 if [ -z "$(git status --porcelain)" ]; then
+    helm package .
     git checkout 0.1.2
     helm package .
     git checkout 0.1.1
@@ -13,7 +14,7 @@ if [ -z "$(git status --porcelain)" ]; then
     git checkout --orphan gh-pages
     git reset
     helm repo index . --url https://tendant.github.io/simple-app
-    git add index.yaml simple-app-0.1.1.tgz simple-app-0.1.2.tgz
+    git add index.yaml simple-app-0.1.1.tgz simple-app-0.1.2.tgz simple-app-$VERSION.tgz
     git commit -am "Update release"
     git clean -df
     git push -f origin gh-pages
